7be67d4015a18968a643c0d7e989dd93278e2a0f
Abhishek <abhishekkanawade2000@gmail.com>
2024-03-24 13:47:11 +0530

    commited

--- sample_pipeline.groovy
+++ sample_pipeline.groovy
@@ -16,7 +16,7 @@ pipeline {
         }
          stage('test') {
             steps {
-                /opt/apache-maven-3.9.6/bin/mvn sonar:sonar -Dsonar.projectKey=Calculator -Dsonar.host.url=http://3.108.196.130:9000/ -Dsonar.login=62dabd732b94780b83e77bce03afa0e9ab5805b9
+                /opt/apache-maven-3.9.6/bin/mvn sonar:sonar -Dsonar.projectKey=Calculator -Dsonar.host.url=http://3.108.196.130:9000 -Dsonar.login=62dabd732b94780b83e77bce03afa0e9ab5805b9
                 echo 'test stage'
             }
         }
