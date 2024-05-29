9f080a83f86e8a2d85766a474defcb229f724815
Abhishek <abhishekkanawade2000@gmail.com>
2024-05-29 19:38:34 +0530

    commited

--- sample_pipeline.groovy
+++ sample_pipeline.groovy
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('pull') {
             steps {
-                git branch: 'main', credentialsId: '92b8381e-6fbd-42eb-afc4-fa275426bc1c', url: 'https://github.com/Abhishekkanawade20/Jenkins.git'
+                git branch: 'main', url: 'https://github.com/Abhishekkanawade20/Jenkins.git'
                 echo "hello"
             }
         }
